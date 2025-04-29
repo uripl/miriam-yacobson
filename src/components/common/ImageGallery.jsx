aefce7a20173eb13a8468134ddf3a6bc48935e3f
uripl <uriplesser@gmail.com>
2025-04-29 10:16:39 +0300

    Update ImageGallery.jsx
    line 2

--- src/components/common/ImageGallery.jsx
+++ src/components/common/ImageGallery.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import '../styles/ImageGallery.css';
+import '../styles/components.css';
 
 /**
  * קומפוננטת גלריית תמונות - מציגה אוסף תמונות עם אפשרות להגדלה
