c2be35d76bb1850ff4cb137ced4111a69ef4938b
uripl <uriplesser@gmail.com>
2025-05-08 08:40:07 +0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,15 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/globals.css';
-import { mapboxWorker } from './mapbox-worker';
-
-// אתחול העובדים של Mapbox
-if (typeof window !== 'undefined') {
-  mapboxWorker.initialize();
-}
-
-// מניעת אתחול כפול במצב Strict Mode של React
-window.MAPBOX_INITIALIZED = false;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
