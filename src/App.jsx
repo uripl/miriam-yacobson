9932220e380bef6f228646adf5c0a9596e073f8e
uripl <uriplesser@gmail.com>
2025-05-12 14:16:20 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 // קומפוננטת מבנה
 import Layout from './components/layout/Layout';
